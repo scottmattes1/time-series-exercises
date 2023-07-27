4a139db6ae37b2357c4ab0a6de67309391f800d4
Scott Mattes <s.scott.mattes@gmail.com>
2023-07-27 13:04:32 -0500

    updated prepare.py

--- prepare.py
+++ prepare.py
@@ -1,5 +1,5 @@
 
-###################### INITIAL IMPORTS #######################
+############################## INITIAL IMPORTS ##############################
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -21,7 +21,7 @@ warnings.filterwarnings("ignore")
 
 
 
-###################### ACQUIRE SALES DATA FUNCTION #######################
+###################### ACQUIRE SALES DATA FUNCTION ##########################
 
 def acquire_sales_data():
     """
@@ -33,31 +33,23 @@ def acquire_sales_data():
     df = df.rename(columns={'sale_date': 'date'})
     df= df.drop(columns='Unnamed: 0')
 
-    # Convert the date column to datetime data
-    df.date = pd.to_datetime(df.date)
-
-    # Plot sale_amount
-    sns.displot(df.sale_amount)
-
-    # Plot item_price
-    sns.displot(df.item_price)
-
+    return df
+    
+    
+def date_set_index(df, col):
+    """
+    Renames the date column to 'date' and sets it as the index of the dataframe. Returns the updated dataframe.
+    """
+    # Rename the designated columns 'date'
+    df = df.rename(columns={col: 'date'})
+    
     # Set the date as the index
     df = df.set_index('date')
 
-    # Set a column that carries the name of the month of the observation
-    df['month'] = df.index.month_name()
-
-    # Set a column that carries the name of the day of the observation
-    df['day_of_week'] = df.index.day_name()
-
-    # Multiply sale_amount by item_price and assign to a column called sales_total
-    df['sales_total'] = df.sale_amount * df.item_price
-    
     return df
 
 
-###################### ACQUIRE GERMAN DAILY FUNCTION #######################
+###################### ACQUIRE GERMAN DAILY FUNCTION #########################
 
 def acquire_german_daily():
     """
@@ -81,28 +73,38 @@ def time_graph_all_columns(df):
         plt.show()
 
 
-###################### ADD MONTH YEAR FUNCTION #######################
+###################### ADD MONTH YEAR FUNCTION ###############################
 
 def add_month_year(df):
     """
     Adds a month and year column. to a dataframe with a TimeIndex. Outputs the updated dataframe
     """
+    # Get date values from the time index and assign it to a column called month
     df['month'] = df.index.month_name()
+    
+    # Get date values from the time index and assign it to a column called year
     df['year'] = df.index.year
+    
     return df
 
 
-###################### CHECK TIME NULLS FUNCTION #######################
+###################### CHECK TIME NULLS FUNCTION #############################
 
-def check_time_nulls(timeindex):
+def check_time_nulls(df):
     """
     Checks the time range of the data and compares it against the number of the observations
     """
-    print('Number of rows:', timeindex.nunique())
-    n_days = timeindex.max() - timeindex.min() + pd.Timedelta('1d')
-    print(f"Number of days between first and last day:", n_days)
-    print(df.index.value_counts().value_counts())
+    # Print the number of rows
+    print('Number of rows:', df.index.nunique())
     
+    # Print the number of value
+    print(f'Value counts: {df.index.value_counts().value_counts()}')
+    
+    # Print the range between the first observation and the last observation
+    n_days = timeindex.max() - df.index.min() + pd.Timedelta('1d')
+    print(f"Number of days between first and last day:", n_days)
+    
+
     
     
     
